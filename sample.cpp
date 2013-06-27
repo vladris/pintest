6bc60270c8783ab95607777a2db71e4ce2c82a27
Vlad Riscutia <riscutiavlad@gmail.com>
2013-06-27 16:53:02 -0700

    Prototype/proof of concept

---
+++ sample.cpp
@@ -0,0 +1,44 @@
+// test.hpp proof of concept
+//
+// Copyright (c) 2013  Vlad Riscutia
+#include "test.hpp"
+
+// Defines a test group
+TEST_GROUP(group1)
+{
+	// Fixture setup
+	TEST_SETUP()
+	{
+		printf("setup\n");
+	}
+
+	// A test
+	TEST(foo)
+	{
+		printf("tests foo!\n");
+	}
+
+	// Another test
+	TEST(bar)
+	{
+		printf("tests bar!\n");
+	}
+}
+
+// Defines another test group
+TEST_GROUP(group2)
+{
+	// Fixture teardown
+	TEST_TEARDOWN()
+	{
+		printf("teardown\n");
+	}
+
+	// A test
+	TEST(baz)
+	{
+		printf("tests baz!\n");
+	}
+}
+
+// Run everything with Tests::getInstance().run()
---
+++ test.hpp
@@ -0,0 +1,111 @@
+// test.hpp proof of concept
+//
+// Copyright (c) 2013  Vlad Riscutia
+
+#include <stdio.h>
+#include <vector>
+#include <memory>
+
+// Runable
+struct Runable { virtual void run() = 0; };
+
+// Base test class
+struct BaseTest : Runable
+{
+	virtual void setup() = 0;
+	virtual void test() = 0;
+	virtual void teardown() = 0;
+
+	void run() override
+	{
+		setup();
+		test();
+		teardown();
+	}
+};
+
+// Test register
+class Tests : Runable
+{
+public:
+	// This is a singleton
+	static Tests& getInstance()
+	{
+		static Tests instance;
+
+		return instance;
+	}
+
+	// Add a test group
+	void add(Runable *reg)
+	{
+		groups.push_back(reg);
+	}
+
+	// Run all tests
+	void run() override
+	{
+		for (auto group : groups)
+		{
+			group->run();
+		}
+	}
+
+private:
+	// No public constructors
+	Tests() { };
+	Tests(Tests const&) { };
+	void operator=(Tests const&);
+
+	// Test groups
+	std::vector<Runable*> groups;
+};
+
+// Represents a group of tests
+class TestGroup : Runable
+{
+public:
+	TestGroup()
+	{
+		// Adds itself to the test register
+		Tests::getInstance().add(this);
+	}
+
+	// Add a test
+	void add(BaseTest *test)
+	{
+		tests.push_back(test);
+	}
+
+	// Run all tests in group
+	void run()
+	{
+		for (auto test : tests)
+		{
+			test->run();
+		}
+	}
+private:
+	std::vector<BaseTest*> tests;
+};
+
+// Test groups are mapped to namespaces
+#define TEST_GROUP(name)		namespace name { static TestGroup testGroup; } namespace name
+
+// Setup and teardown
+#define TEST_SETUP()	struct Setup { static void run(); }; void Setup::run()
+#define TEST_TEARDOWN() struct Teardown { static void run(); }; void Teardown::run()
+
+// Test (automatically instantiates itself and adds itself to test group)
+#define TEST(name)	struct name : public BaseTest \
+					{ \
+					    name() { testGroup.add(this); } \
+						void setup() { Setup::run(); } \
+						void teardown() { Teardown::run(); } \
+						void test(); \
+					} _ ## name; \
+					void name::test()
+
+// Setup and teardown to be used if the test group doesn't define its own
+struct Setup { static void run() {} };
+struct Teardown { static void run() {} };
