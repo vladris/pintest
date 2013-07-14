8a8bca371567dab5a991d55b0509b81660a46029
Vlad Riscutia <vladris@vladris-vm.b3be10a93fa64b0284bf5dcb84e01d98.vladris-vm.3933866493.uswest.internal.cloudapp.net>
2013-07-14 00:15:55 +0000

    First test

--- test/test_native.py
+++ test/test_native.py
@@ -8,10 +8,22 @@
     :license: MIT License
 '''
 from testhpp_runner import discovery, executor, interop
-import unittest
+from unittest import TestCase
+from os import path as path
 
 
+class NativeTests(TestCase):
+    def test_discovery(self):
+        '''
+        Tests test discovery
+        '''
+        modules = discovery.discover('.')        
 
-def test_discovery():
-    modules = discovery.discover('.')        
-    print(modules)
+        # should discover one module 
+        self.assertEquals(1, len(modules))
+
+        module = modules[0]
+
+        # make sure the right module was found 
+        filename = path.basename(module.filename).lower()
+        self.assertTrue(filename in ['libtest.so', 'test.dll'])
